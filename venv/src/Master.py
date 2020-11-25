5256d2adf11d1e8dae08c92ad8807d0202aa117e
Brandt Severson <baseverson@gmail.com>
2020-11-24 17:24:54 -0800

    Add automatic start and stop times

--- venv/src/Master.py
+++ venv/src/Master.py
@@ -70,13 +70,8 @@ class MasterController:
             now = datetime.now().time()
 
             # Generate start and end times
-            startTime = now.replace(
-                hour=self.cfg.getStartTimeHour(), minute=self.cfg.getStartTimeMinute(), second=0, microsecond=0)
-            endTime = now.replace(
-                hour=self.cfg.getEndTimeHour(),
-                minute=self.cfg.getEndTimeMinute(),
-                second=0,
-                microsecond=0)
+            startTime = now.replace(hour=self.cfg.getStartTimeHour(), minute=self.cfg.getStartTimeMinute(), second=0, microsecond=0)
+            endTime = now.replace(hour=self.cfg.getEndTimeHour(), minute=self.cfg.getEndTimeMinute(), second=0, microsecond=0)
 
             # Make sure we're after the start time and before the stop time
             if (now > startTime & now < endTime):
